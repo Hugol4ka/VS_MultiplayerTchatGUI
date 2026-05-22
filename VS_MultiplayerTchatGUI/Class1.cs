f645070040968ab39bc578cd0bc237a2f37b524d
Hugol4ka <hugolaca12300@gmail.com>
2026-05-22 14:25:42 +0200

    Add initial project files and configurations for .NET 8.0
    - Created assembly info and attributes for VS_MultiplayerTchatGUI.
    - Added sourcelink support for debugging.
    - Included necessary DLL references and cache files for project compilation.
    - Generated MSBuild editor config for .NET 8.0.
    - Established global usings for common namespaces.
    - Set up assets cache and file list for project structure.

--- VS_MultiplayerTchatGUI/Class1.cs
+++ VS_MultiplayerTchatGUI/Class1.cs
@@ -1,6 +1,84 @@
-﻿namespace VS_MultiplayerTchatGUI;
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
 
-public class Class1
+namespace VS_MultiplayerTchatGUI
 {
+    public class VS_MultiplayerTchatGUIModSystem : ModSystem
+    {
+        private ICoreServerAPI? apiServeur;
 
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+            apiServeur = api;
+
+            api.Event.PlayerChat += OnPlayerChat;
+        }
+
+        private void OnPlayerChat(IServerPlayer joueur, int canalId, ref string message, ref string displayName, BoolRef consume)
+        {
+
+            string codeClasse = joueur.Entity?.WatchedAttributes.GetString("characterClass") ?? "commoner";
+            string prefix = "";
+
+            switch (codeClasse)
+            {
+                case "commoner":
+                    prefix = "[#9b9ea1][Citoyen][#FFFFFF] ";
+                    break;
+                case "archivist":
+                    prefix = "[#f1c40f][Archiviste][#FFFFFF] ";
+                    break;
+                case "blackguard":
+                    prefix = "[#d9381e][Garde Noir][#FFFFFF] ";
+                    break;
+                case "brickmaker":
+                    prefix = "[#d35400][Briquetier][#FFFFFF] ";
+                    break;
+                case "butcher":
+                    prefix = "[#e74c3c][Boucher][#FFFFFF] ";
+                    break;
+                case "clockmaker":
+                    prefix = "[#00cea6][Horloger][#FFFFFF] ";
+                    break;
+                case "farmhand":
+                    prefix = "[#2ecc71][Fermier][#FFFFFF] ";
+                    break;
+                case "florist":
+                    prefix = "[#ff69b4][Fleuriste][#FFFFFF] ";
+                    break;
+                case "forester":
+                    prefix = "[#27ae60][Forestier][#FFFFFF] ";
+                    break;
+                case "hunter":
+                    prefix = "[#e67e22][Chasseur][#FFFFFF] ";
+                    break;
+                case "malefactor":
+                    prefix = "[#9b59b6][Malfaiteur][#FFFFFF] ";
+                    break;
+                case "messenger":
+                    prefix = "[#3498db][Messager][#FFFFFF] ";
+                    break;
+                case "quarrier":
+                    prefix = "[#7f8c8d][Carrier][#FFFFFF] ";
+                    break;
+                case "spelunker":
+                    prefix = "[#a0522d][Spéléologue][#FFFFFF] ";
+                    break;
+                case "tailor":
+                    prefix = "[#e84393][Tailleur][#FFFFFF] ";
+                    break;
+                case "vintner":
+                    prefix = "[#800020][Vigneron][#FFFFFF] ";
+                    break;
+                default:
+                    prefix = $"[#9b9ea1][{codeClasse}][#FFFFFF] ";
+                    break;
+            }
+
+            displayName = prefix + displayName;
+        }
+    }
 }
